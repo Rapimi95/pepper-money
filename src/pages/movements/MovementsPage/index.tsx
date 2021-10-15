5bc9e480258595d7013ff61d769413c1ebda0dde
Rafael Pimienta <rafael.pimienta@globant.com>
2021-10-15 09:28:36 -0500

    Fix: Add useCategories to movements page

--- src/pages/movements/MovementsPage/index.tsx
+++ src/pages/movements/MovementsPage/index.tsx
@@ -2,10 +2,12 @@ import ListOfMovements from '../../../components/ListOfMovements';
 import AddMovementFAB from '../../../components/AddMovementFAB';
 import MainLayout from '../../../components/MainLayout';
 import useMovements from '../../../hooks/useMovements';
+import useCategories from '../../../hooks/useCategories';
 import BottomNavBar from '../../../components/BottomNavBar';
 
 const MovementsPage = () => {
     useMovements();
+    useCategories();
 
     return (
         <MainLayout>
